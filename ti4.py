226a552b5905fe5990109d083c862c9d73b3dee4
Shy <shy@users.noreply.github.com>
2021-05-29 21:08:06 -0400

    Init

--- ti4.py
+++ ti4.py
@@ -5,3 +5,10 @@ from app.models import User, Game
 @app.shell_context_processor
 def make_shell_context():
     return {"db": db, "User": User, "Game": Game}
+
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
